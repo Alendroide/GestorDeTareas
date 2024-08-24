2a384922b4788e4fa93b5dde9abdb0b2bad3d603
Pepe <alendroideyt@gmail.com>
2024-08-23 19:18:07 -0500

    Vistas creadas

--- tareas/views.py
+++ tareas/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Tarea
+from .forms import TareaForm
 
 # Create your views here.
+def crearTarea(request):
+    if request.method == 'POST':
+        form = TareaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listarTareas')
+    form = TareaForm()
+    return render(request,'crearTarea.html',{'form':form})
+
+def listarTareas(request):
+    tareas = Tarea.objects.all()
+    return render(request,'listarTareas.html',{'tareas':tareas})
