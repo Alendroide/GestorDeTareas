dba6910131a3518a02c33c675fa2cba2536cb8e5
Pepe <alendroideyt@gmail.com>
2024-08-23 19:15:26 -0500

    Creacion modelo y formulario

--- tareas/models.py
+++ tareas/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Tarea(models.Model):
+    estados = [('terminada','Terminada')('pendiente','Pendiente')]
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField(max_length=500)
+    estado = models.CharField(max_length=20,choices=estados,default='pendiente')
+    def __str__(self):
+        return self.titulo
